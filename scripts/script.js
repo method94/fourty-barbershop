e1e9bc6b0064ed35cc4467f9a6eca9671a0b853f
metropol68 <abs_hakan50@hotmail.de>
2025-06-03 14:53:43 +0200

    final ohne impressum

--- scripts/script.js
+++ scripts/script.js
@@ -47,3 +47,16 @@ menuLinks.forEach(link => {
   });
 });
 
+/* SLIDESHOW */
+
+  document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#image-slider', {
+      type    : 'loop',
+      perPage : 1,
+      autoplay: true,
+      interval: 3000,
+      pauseOnHover: true,
+      pagination: true,
+      arrows: true,
+    }).mount();
+  });
